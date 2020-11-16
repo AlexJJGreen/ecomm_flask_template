f4ce1940acb38a80e66bd48b86b835c23a36edbb
alexjjgreen <alexjjgreen@gmail.com>
2020-11-16 17:43:50 +0100

    add index and login form pages

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,18 @@
 from flask import current_app as app
 from flask import render_template
+from application.forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("base.html")
+    title = "Home"
+    return render_template("index.html", title=title)
+
+@app.route("/login")
+def login(methods=["GET", "POST"]):
+    form = LoginForm()
+    return render_template("login.html", form=form)
+
+@app.route("/products")
+def products():
+    return render_template("products.html")
